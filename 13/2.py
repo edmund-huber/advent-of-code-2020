9b954d7666dc830087610ad6b0362705a1e86052
Edmund Huber <me@ehuber.info>
2020-12-12 22:29:49 -0800

    i copied in the py3 version of the CRT from rosettacode and it worked lol

--- 13/2.py
+++ 13/2.py
@@ -21,14 +21,15 @@ for bus, off in zip(busses, offs):
     rem.append(-off)
 print(rem)
 
+from functools import reduce
 def chinese_remainder(n, a):
     sum = 0
     prod = reduce(lambda a, b: a*b, n)
- 
     for n_i, a_i in zip(n, a):
-        p = prod / n_i
+        p = prod // n_i
         sum += a_i * mul_inv(p, n_i) * p
     return sum % prod
+ 
  
  
 def mul_inv(a, b):
@@ -36,7 +37,7 @@ def mul_inv(a, b):
     x0, x1 = 0, 1
     if b == 1: return 1
     while a > 1:
-        q = a / b
+        q = a // b
         a, b = b, a%b
         x0, x1 = x1 - q * x0, x0
     if x1 < 0: x1 += b0
